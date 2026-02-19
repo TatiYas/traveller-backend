6ccabc47875678b8279d48a1de7d6c21928ba66f
TatiYas <iasinovska_lawyer@ukr.net>
2026-02-20 00:19:23 +0100

    authController.js+services/auth.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,97 @@
+import { registerUser } from '../services/auth.js';
+import { loginUser } from '../services/auth.js';
+import { logoutUser } from '../services/auth.js';
+import { createSession } from '../services/auth.js';
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/time.js';
+import { SessionsCollection } from '../database/models/session.js';
+import { refreshUsersSession } from '../services/auth.js';
+
+const setupSession = (res, session) => {
+  // eslint-disable-next-line no-undef
+  const isProd = process.env.NODE_ENV === 'development';
+
+  const cookieOptions = {
+    httpOnly: true,
+    sameSite: isProd ? 'none' : 'lax',
+    path: '/',
+    secure: isProd,
+  };
+
+  res.cookie('accessToken', session.accessToken, {
+    ...cookieOptions,
+    expires: new Date(Date.now() + FIFTEEN_MINUTES),
+  });
+
+  res.cookie('refreshToken', session.refreshToken, {
+    ...cookieOptions,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+
+  res.cookie('sessionId', session._id, {
+    ...cookieOptions,
+    expires: new Date(Date.now() + THIRTY_DAYS),
+  });
+};
+
+export const registerUserController = async (req, res) => {
+  const user = await registerUser(req.body);
+
+  const newSession = await createSession(user._id);
+  setupSession(res, newSession);
+
+  res.status(201).json({
+    status: 201,
+    message: 'User registered successfully',
+    data: user,
+  });
+};
+
+export const loginUserController = async (req, res) => {
+  const user = await loginUser(req.body);
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+  setupSession(res, newSession);
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: user,
+  });
+};
+
+export const logoutUserController = async (req, res) => {
+  // eslint-disable-next-line no-undef
+  const isProd = process.env.NODE_ENV === 'production';
+
+  if (req.cookies.sessionId) {
+    await logoutUser(req.cookies.sessionId);
+  }
+
+  const clearOptions = {
+    httpOnly: true,
+    sameSite: 'lax',
+    path: '/',
+    secure: isProd,
+  };
+
+  res.clearCookie('accessToken', clearOptions);
+  res.clearCookie('refreshToken', clearOptions);
+  res.clearCookie('sessionId', clearOptions);
+
+  res.status(204).send();
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+  });
+};
---
+++ src/services/auth.js
@@ -0,0 +1,72 @@
+import { randomBytes } from 'crypto';
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/time.js';
+import { SessionsCollection } from '../database/models/session.js';
+import { UsersCollection } from '../database/models/user.js';
+import bcrypt from 'bcryptjs';
+import createHttpError from 'http-errors';
+
+export const createSession = async (userId) => {
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return SessionsCollection.create({
+    userId,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
+  });
+};
+
+export const registerUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (user) throw createHttpError(409, 'Email in use');
+
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+  return user;
+};
+
+export const logoutUser = async (sessionId) => {
+  await SessionsCollection.deleteOne({ _id: sessionId });
+};
+
+export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
+  const session = await SessionsCollection.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+  const userId = session.userId;
+
+  await SessionsCollection.deleteOne({ _id: sessionId, refreshToken });
+
+  const newSession = await createSession(userId);
+
+  return newSession;
+};
