967ee9d57b7483134559afaaa1e948cdfd922149
TatiYas <iasinovska_lawyer@ukr.net>
2026-02-19 23:52:03 +0100

    middleware/authenticate.js+routes/authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,26 @@
 import { Router } from 'express';
-
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  registerUserController,
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+} from '../controllers/auth.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  registerUserController,
+);
 
-router.post('/auth/register');
-//...
+router.post('/login', validateBody(loginUserSchema), loginUserController);
+
+router.post('/logout', authenticate, logoutUserController);
+
+router.post('/refresh-session', refreshUserSessionController);
 
 export default router;
