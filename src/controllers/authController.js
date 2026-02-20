5ab8cb37cb5035ed2ef5bdce55116b90cfff84b9
TatiYas <iasinovska_lawyer@ukr.net>
2026-02-20 17:21:51 +0100

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import { refreshUsersSession } from '../services/auth.js';
 
 const setupSession = (res, session) => {
   // eslint-disable-next-line no-undef
-  const isProd = process.env.NODE_ENV === 'development';
+  const isProd = process.env.NODE_ENV === 'productio';
 
   const cookieOptions = {
     httpOnly: true,
